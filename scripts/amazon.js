a61a4f8e07dba96eeecdc1f3d26b49c6534f05cd
harshil05133 <rathodharshil05133@gmail.com>
2025-03-20 18:48:54 -0400

    make total cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -81,7 +81,16 @@ document.querySelectorAll('.js-add-to-cart-button').forEach((button) => {
       });
     }
 
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    })
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
     console.log(cart);
+    console.log(cartQuantity);
   });
 });
 
