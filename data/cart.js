de118a4ad0342126f979cd6cf85160d3a2cf5d98
harshil05133 <rathodharshil05133@gmail.com>
2025-03-21 16:29:44 -0400

    moved add to cart into cart.js, imported products from products.js as well

--- data/cart.js
+++ data/cart.js
@@ -1,2 +1,22 @@
 
 export let cart = [];
+
+export function addToCart(productId){
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if(productId === cartItem.productId){
+      matchingItem = cartIitem;
+    }
+  });
+
+  if (matchingItem){
+    matchingItem.quantity++;
+  }
+  else{
+    cart.push({
+      productId: productId,
+      quantity: 1
+    });
+  }
+}
